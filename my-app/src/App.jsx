66c6a70c5a318d047635efdd50d05a7791c6a17c
karimmoustamid <moustamid.teck@gmail.com>
2025-05-04 18:12:53 -0600

    Add App component and update entry point to index.jsx

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      {/* Displaying a heading */}
+      <h1>Hello, world!</h1>
+      {/* Displaying a paragraph */}
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -4,23 +4,14 @@ import React from 'react';
 // Importing ReactDOM to interact with the DOM and render React components
 import ReactDOM from 'react-dom/client';
 
+import App from './App.jsx';
+
 // Selecting the root DOM element where the React app will be mounted
 const element = document.getElementById('root');
 
 // Creating a root for rendering the React application
 const root = ReactDOM.createRoot(element);
 
-// Defining the main App component
-function App() {
-  return (
-    <div>
-      {/* Displaying a heading */}
-      <h1>Hello, world!</h1>
-      {/* Displaying a paragraph */}
-      <p>This is a simple React application.</p>
-    </div>
-  );
-}
 
 // Rendering the App component into the root DOM element
 root.render(
